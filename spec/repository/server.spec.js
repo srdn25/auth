de6001a52e6ec7ab60fbf733ae050cb5cdeedba9
srdn <srdn2417@gmail.com>
2020-04-13 22:45:10 +0300

    Server repository findBySlug, fingById. Also tests for this methods

--- spec/repository/server.spec.js
+++ spec/repository/server.spec.js
@@ -9,4 +9,20 @@ describe('Server repository', function () {
 
     expect(server).to.have.all.keys(ENTITY.fields);
   });
+
+  it('Find server by ID', async () => {
+    let server = await repository.findById(Server.id);
+    server = server.toJSON();
+
+    expect(server).to.have.all.keys(ENTITY.fields);
+    expect(server).to.deep.equal(Server.toJSON());
+  });
+
+  it('Find server by slug', async () => {
+    let server = await repository.findBySlug(Server.slug);
+    server = server.toJSON();
+
+    expect(server).to.have.all.keys(ENTITY.fields);
+    expect(server).to.deep.equal(Server.toJSON());
+  });
 });
--- src/repository/server.repository.js
+++ src/repository/server.repository.js
@@ -1,7 +1,11 @@
 const psql = require('../psql/models');
 
 const create = (data) => psql.server.create(data);
+const findById = (id) => psql.server.findOne({ where: { id } });
+const findBySlug = (slug) => psql.server.findOne({ where: { slug } });
 
 module.exports = {
   create,
+  findById,
+  findBySlug,
 };
