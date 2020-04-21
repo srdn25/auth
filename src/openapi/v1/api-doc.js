5f8ba68f04f8ce54ee27d115bab76503e89a9d09
srdn <srdn2417@gmail.com>
2020-04-21 21:57:27 +0300

    openapi schema

--- src/openapi/v1/api-doc.js
+++ src/openapi/v1/api-doc.js
@@ -1,20 +1,40 @@
-const OpenApi = require('openapi-backend');
+const OpenApi = require('openapi-backend').default;
 
 const api = new OpenApi({
   definition: {
-    openapi: '3.0.1',
+    openapi: '3.0.2',
     info: {
       title: 'Auth server',
       version: require('../../../package.json').version,
+      contact: {
+        email: 'srdn2417@gmail.com'
+      }
     },
+    tegs: [
+      {
+        name: 'User',
+        description: 'User in system',
+      }
+    ],
     paths: {
       '/api/v1/user': {
         post: {
+          tags: ['user'],
           operationId: 'createUser',
           requestBody: {
+            required: true,
             description: 'User object to create',
             content: {
-              'application/json': {},
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    name: {
+                      type: 'string',
+                    },
+                  }
+                },
+              },
             },
             responses: {
               200: { description: 'OK' },
@@ -26,7 +46,16 @@ const api = new OpenApi({
     handlers: {
       createUser: async (c, ctx) => {
         ctx.body = { operationId: c.operation.operationId }
-      }
+      },
+      validationFail: async (c, ctx) => {
+        ctx.body = { err: c.validation.errors };
+        ctx.status = 400;
+      },
+      notFound: async (c, ctx) => {
+        console.log(c);
+        ctx.body = { err: 'not found' };
+        ctx.status = 404;
+      },
     },
   },
 
