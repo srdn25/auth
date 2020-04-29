550f16d8eaee66e9b477886ce89c04ae7f495bef
srdn <srdn2417@gmail.com>
2020-04-29 18:18:50 +0300

    Init server router

--- src/router/v1/index.js
+++ src/router/v1/index.js
@@ -1,8 +1,15 @@
 const Router = require('koa-router');
-const userRouter = require('./user.router');
-
+const exportAll = require('../../../scripts/export_all_from_folder');
 const router = new Router({ prefix: '/v1' });
 
-router.use(userRouter.routes(), userRouter.allowedMethods());
+const routes = exportAll({
+  folder: __dirname,
+  filter: '.router.js',
+});
+
+for (const [name, route] of Object.entries(routes)) {
+  router.use(route.routes(), route.allowedMethods());
+  console.log(`Router init ${name}`)
+}
 
 module.exports = router;
--- src/router/v1/server.router.js
+++ src/router/v1/server.router.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router');
 
-const router = new Router({ prefix: '/user' });
+const router = new Router({ prefix: '/server' });
 
 // Create server
 router.post('/', (ctx) => {
   const data = ctx.requrest.body;
+  ctx.body = data;
 });
 
 module.exports = router;
