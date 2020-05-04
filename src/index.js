0ea4576d9aaa44297210ad9f5e36476ff81bdfaa
srdn <srdn2417@gmail.com>
2020-05-04 11:14:42 +0300

    Clear validator

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    ctx.status = err.status || 500;
+    ctx.status = err.status || ctx.status || 500;
     ctx.body = err.message;
     ctx.app.emit('error', err, ctx);
   }
